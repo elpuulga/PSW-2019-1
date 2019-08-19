76467dd302a4b333672679c66cfb3106ac350856
elpuulga <gabriel.langwinski@gmail.com>
2019-08-19 19:38:08 -0300

    Inclusão do primeiro projeto no repositório.

---
+++ aula_01/src/br/edu/udc/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.edu.udc;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		int valor = 100;
+		System.out.println("Primeiro programa java!!!");	//forma de pular linha em java 
+			
+		System.out.printf("mostrando um número %d\n", valor); //forma de pular linha em C
+	}
+
+}
